63702f62815ea36a97c7bdc9e0894043160fa5d2
Sadikul Mobasshir <114331112+MOBASSHIR07@users.noreply.github.com>
2026-03-31 21:38:29 +0600

    dashboard layout skeleton done

--- app/(DashboardLayout)/@sender/sender-dashboard/page.tsx
+++ app/(DashboardLayout)/@sender/sender-dashboard/page.tsx
@@ -1,1 +1,9 @@
-export default function Page() { return <div>Page Content</div> }
+export default function Page() {
+  return (
+    <div className="p-8">
+      <h2 className="text-xl font-black text-white/20 uppercase italic tracking-widest">
+        Nexus Express System: Module Initialized...
+      </h2>
+    </div>
+  );
+}
